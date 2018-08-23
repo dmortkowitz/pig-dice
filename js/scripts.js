f4c7b808756fe8c217f30f52c447b10fc699b1c5
dm <dmortkowitz@gmail.com>
2018-08-23 10:48:16 -0700

    Add additional random text results to winning and losing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 // backend
 var playerOne="";
 var playerTwo="";
-
+var loseTextArray = ["Sorry, but you rolled a 1! Your efforts have returned ill fruit, like a barren land.", "In the land of pigs, rolling a 1 leads to your doom. This game is over.", "You can hear them coming. Soon, they will arrive in their pinstripe suits, their hooves pounding the floor as they take you away. The game has ended. What was it all for?"]
+var winTextArray = ["Winner winner, chicken dinner! It seems that you won, and the game has ended, like all things eventually must. Why are we here?", "You have won the game, and the Boss Pig must keep his word. You are free.", "The night is dark, the future uncertain, but you have earned Boss Pig's respect, and in turn have escaped certain doom this day."];
+var randomLose = loseTextArray[Math.floor(Math.random() * loseTextArray.length)];
+console.log(loseTextArray[Math.floor(Math.random() * loseTextArray.length)])
+var randomWin = winTextArray[Math.floor(Math.random() * winTextArray.length)];
 var rollDice = function() {
 	return Math.floor(6*Math.random())+1;
 }
@@ -17,9 +21,10 @@ function Player(turn) {
 Player.prototype.rollOne = function(){
 	if (this.roll === 1){
 		this.turnScore = 0;
-		alert("Sorry, " + this.playerName + ", but you rolled a 1! Your efforts have returned stark fruit, like a barren land.")
+		// $("#msgCenter").text("Sorry, " + this.playerName + ", but you rolled a 1! Your efforts have returned ill fruit, like a barren land.")
 	// } else if (this.roll === 6) {
 	// 		alert("Winner winner, chicken dinner, " + this.playerName + "! It seems that you won. But to what end?")
+		$("#msgCenter").text(randomLose);
 	} else {
 		this.turnScore += this.roll;
 	}
@@ -28,12 +33,13 @@ Player.prototype.rollOne = function(){
 Player.prototype.hold = function() {
 	this.cumeScore += this.turnScore;
 	this.turnScore = 0;
-	alert("Your turn has come to an end. And so it is, as with all things. Why are we here, " + this.playerName + "?");
+	// alert("Your turn has come to an end. And so it is, as with all things. Why are we here, " + this.playerName + "?");
 }
 
 Player.prototype.chickenDinner = function() {
 	if (this.cumeScore >= 30) {
-		alert("Winner winner, chicken dinner, " + this.playerName + "! It seems that you won. But to what end?")
+		// $("#msgCenter").text("Winner winner, chicken dinner! It seems that you won, and the game has ended, like all things eventually must. Why are we here, " + this.playerName +"?")
+		$("#msgCenter").text(randomWin);
 	}
 }
 
